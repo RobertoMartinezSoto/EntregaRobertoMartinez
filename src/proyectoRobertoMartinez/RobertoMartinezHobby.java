3e59e1177998fe88d878b66f3461fc8e229efa35
Roberto <Roberto@192.168.0.105>
2022-02-21 23:10:45 +0100

    1Commit getterObra

---
+++ src/proyectoRobertoMartinez/RobertoMartinezHobby.java
@@ -0,0 +1,16 @@
+package proyectoRobertoMartinez;
+
+public class RobertoMartinezHobby {
+	public String obra;
+	public String compositor;
+	public String nombreHobby;
+	
+	public RobertoMartinezHobby(String obra, String compositor, String nombreHobby) {
+		this.obra=obra;
+		this.compositor=compositor;
+		this.nombreHobby=nombreHobby;
+	}
+	public String getObra() {
+		return obra;
+	}
+}
